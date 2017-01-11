57c7a0bfc28027bbf46dfb103953dec82fbf4a6d
tralyn <let1@unlv.nevada.edu>
2017-01-11 11:44:06 -0800

    Create solveMeFirst.cpp

---
+++ cpp/Algorithms/Warmup/solveMeFirst.cpp
@@ -0,0 +1,23 @@
+// Problem: https://www.hackerrank.com/rest/contests/master/challenges/solve-me-first/download_pdf?language=English
+// Program: Code that prints the sum calculated and returned by solveMeFirst.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  return a+b;
+}
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
